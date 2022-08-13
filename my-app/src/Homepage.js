7522ca9060eb10a453fde2f4e773fafb62e8678e
Teerasuk Theanchai <donwallet58@gmail.com>
2022-08-13 19:37:25 +0700

    update navbar

--- my-app/src/Homepage.js
+++ my-app/src/Homepage.js
@@ -1,9 +1,60 @@
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import './Homepage.css';
+import { useEffect, useState } from 'react';
+
 export default function Homepage(){
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(
+                "http://localhost:8080/api/service",
+                {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                        'Content-Type': 'application/json',
+                        Authorization: "Bearer " + localStorage.getItem("access_token")
+                    }
+                }
+            );
+
+            let json = await response.json();
+            setServices(json.data);
+            console.log(services);
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <Navigation />
+            
+            <div className="container">
+                <div className='homepage-picture'>
+                    <img src={`http://localhost:8080/images/Appointment.png`} width={"100%"}/>
+                    <button className='btn btn-appointment'>นัดหมายบริการ</button>
+                </div>
+
+                <div className="Services text-center p-5">
+                    <h1 className="Text-title">บริการของคลินิก</h1>
+                    <div className="Services-Items">
+                        {
+                            services.map(item => (
+                                <div className="items d-inline-block mx-5 text-center">
+                                    <img src={`http://localhost:8080/images/${item.service_image}`}/>
+                                    <h6 className='mt-2 fs-4'>{item.service_name}</h6>
+                                </div>
+                            ))
+                        }
+
+                    </div>
+                </div>
+            </div>
+
             <Footer/>
         </>
     )
--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -17,21 +17,24 @@ export default function Navigation(){
                      <p><i class="fa-solid fa-phone me-2"></i>081-622-7293</p>
                 </div>
             </div>
-            <div className='Navbar d-flex justify-content-between p-2'>
-                <div className="Brand ms-5">
-                    <img src={`http://localhost:8080/images/Logo2.png`}/>
-                </div>
-                <div className="Menu-list me-3">
-                    <Navbar bg="light" variant="light">    
+
+            <div className='Navbar'>
+                <Navbar className="d-flex justify-content-between" bg="light" variant="light">    
+                    <div>
+                        <Navbar.Brand href="#home"><img src={`http://localhost:8080/images/Logo2.png`}/></Navbar.Brand>
+                    </div>
+
+                    <div>
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">หน้าแรก</Nav.Link>
+                            <Nav.Link bsPrefix="" href="#home">หน้าแรก</Nav.Link>
                             <Nav.Link href="#about">เกี่ยวกับเรา</Nav.Link>
                             <Nav.Link href="#services">บริการของเรา</Nav.Link>
                             <Nav.Link href="#appointment">นัดหมาย</Nav.Link>
                             <Nav.Link className="Btn-login" href="login"><i class="fa-solid fa-right-to-bracket me-2"></i>เข้าสู่ระบบ</Nav.Link>
                         </Nav>
-                    </Navbar>
-                </div>
+                    </div>
+                        
+                </Navbar>
             </div>
         </div>
     )
