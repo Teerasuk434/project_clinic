fc211aff2caa87338223ec70e554fbf5a7e78894
ปาริฉัตร ละม้ายสกุล <lamai.p2000@gmail.com>
2022-08-20 17:28:07 +0700

    Update Employee.js

--- server/libs/Employee.js
+++ server/libs/Employee.js
@@ -9,26 +9,21 @@ module.exports = {
 
         return await pool.query(sql);
     },
-    updateEmployee: async (pool, user_id,username, password, role_id,status) =>{
-        if(status == true){
-            var sql = "UPDATE employee SET username = ?,password = MD5(?),role_id = ? WHERE user_id = ?";
-        }else{
-            console.log("else");
-            var sql = "UPDATE employee SET username = ?,password = ?,role_id = ? WHERE user_id = ?";
-        }
-        sql = mysql.format(sql, [username,password,role_id,user_id]);
+    updateEmployee: async (pool, emp_id,emp_fname, emp_lname, emp_address, emp_tel, emp_salary, emp_position_id) =>{
+        var sql = "UPDATE employee SET emp_fname = ?, emp_lname = ?, emp_address = ?, emp_tel = ?, emp_salary = ?, emp_position_id = ? WHERE emp_id = ?";
+        sql = mysql.format(sql, [emp_fname, emp_lname, emp_address, emp_tel, emp_salary, emp_position_id, emp_id]);
+        console.log(sql);
+        return await pool.query(sql);
+    },
+    deleteEmployee: async (pool, emp_id) => {
+        var sql = "DELETE FROM employee WHERE emp_id = ?";
+        sql = mysql.format(sql, [emp_id]);
 
         return await pool.query(sql);
     },
-    deleteEmployee: async (pool, user_id) => {
-        var sql = "DELETE FROM employee WHERE user_id = ?";
-        sql = mysql.format(sql, [user_id]);
-
-        return await pool.query(sql);
-    },
-    getByUserId: async (pool, user_id) => {
-        var sql = "SELECT * FROM employee WHERE user_id = ?";
-        sql = mysql.format(sql, [user_id]);
+    getByEmpId: async (pool, emp_id) => {
+        var sql = "SELECT * FROM employee WHERE emp_id = ?";
+        sql = mysql.format(sql, [emp_id]);
 
         return await pool.query(sql);
     }
