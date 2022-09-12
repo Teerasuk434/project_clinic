5aed4e040cf6b8cf4b943a995166d713c028449a
Teerasuk Theanchai <donwallet58@gmail.com>
2022-09-12 14:56:10 +0700

    update appointmentList

--- my-app/src/components/Modal.js
+++ my-app/src/components/Modal.js
@@ -1,5 +1,6 @@
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal,Form,Row,Col} from "react-bootstrap";
 import { useState } from "react";
+
 
 export function ShowPaymentModal(props) {
     return (
@@ -23,7 +24,7 @@ export function ShowPaymentModal(props) {
 
 export function ShowAppointmentDetails(props) {
 
-    let note = props.data.note;
+    let appoint_status = props.data.appoint_status;
 
     const [showImageModal, setShowImageModal] = useState(false);
     const [paymentTitleModal, setPaymentTitleModal] = useState("");
@@ -39,9 +40,6 @@ export function ShowAppointmentDetails(props) {
         setShowImageModal(false);
     }
 
-    const ShowPayment = () => {
-        
-    }
     return (
         <>
             <Modal show={props.show} onHide={props.onClose}>
@@ -57,7 +55,82 @@ export function ShowAppointmentDetails(props) {
                         <p><b>วันที่ :</b> {new Date(props.data.date).toLocaleDateString()}</p>
                         <p><b>เวลา :</b> {props.data.time}</p>
                         <p><b>ห้อง :</b> {props.data.room_name}</p>
-                        {note != "ไม่มี" &&
+                        {appoint_status == "รอแก้ไข" &&
+                        <>
+                            <p className="d-inline-block"><b>หมายเหตุ :</b></p> <p className="text-danger d-inline-block">{props.data.note}</p>
+                        </>                        
+                        }
+                    </div>
+
+                </Modal.Body>
+                <Modal.Footer>
+
+                    <Button variant="success" onClick={onClickShow}>ข้อมูลการชำระค่ามัดจำ</Button>
+
+                    <Button variant="danger" onClick={props.onClose}>ปิด</Button>
+
+                </Modal.Footer>
+
+            </Modal>
+            <ShowPaymentModal 
+            show={showImageModal}
+            onClose={onCloseImageModal}
+            title={paymentTitleModal}
+            paymentImage={paymentImageModal}
+            />
+        </>
+    );
+};
+
+export function ShowAppointmentForm(props) {
+
+    let appoint_status = props.data.appoint_status;
+
+    const [showImageModal, setShowImageModal] = useState(false);
+    const [paymentTitleModal, setPaymentTitleModal] = useState("");
+    const [paymentImageModal, setPaymentImageModal] = useState("");
+    const [pet_id,setPetId] = useState(0);
+
+    const onClickShow = () => {
+        setShowImageModal(true);
+        setPaymentImageModal(props.data.payment_image);
+        setPaymentTitleModal("รูปภาพการชำระเงิน");
+    }
+
+    const onCloseImageModal = () => {
+        setShowImageModal(false);
+    }
+
+    return (
+        <>
+            <Modal show={props.show} onHide={props.onClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{props.title}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <div>
+                        <p className="d-inline-block"><b>สัตว์เลี้ยง :</b> </p>
+                        <p className="d-inline-block">
+                            <Form.Select
+                                value={pet_id}
+                                onChange={(e) => setPetId(e.target.value)}
+                                required>
+                                <option label="กรุณาเลือกสัตว์เลี้ยง"></option> 
+                                {
+                                props.pets.map(item => (
+                                    <option key={item.pet_id} value={item.pet_id}> 
+                                    {item.pet_name} </option>
+                                ))
+                                }
+                            </Form.Select>
+                        </p>
+                        <p><b>อาการเบื้องต้น :</b> {props.data.symtoms}</p>
+                        <p><b>บริการ :</b> {props.data.service_name}</p>
+                        <p><b>วันที่ :</b> {new Date(props.data.date).toLocaleDateString()}</p>
+                        <p><b>เวลา :</b> {props.data.time}</p>
+                        <p><b>ห้อง :</b> {props.data.room_name}</p>
+                        {appoint_status == "รอแก้ไข" &&
                         <>
                             <p className="d-inline-block"><b>หมายเหตุ :</b></p> <p className="text-danger d-inline-block">{props.data.note}</p>
                         </>                        
--- my-app/src/components/customer/AppointmentItem.js
+++ my-app/src/components/customer/AppointmentItem.js
@@ -1,24 +1,48 @@
     import { Link } from "react-router-dom";
-    import { ShowAppointmentDetails } from "../Modal";
-    import { useState } from "react";
-import { Button } from "react-bootstrap";
+    import { ShowAppointmentDetails,ShowAppointmentForm } from "../Modal";
+    import { useState,useEffect } from "react";
+    import { Button } from "react-bootstrap";
+    import { API_POST } from "../../api";
 
     export default function AppointmentItem(props) {
 
     let appoint_status = props.data.appoint_status;
 
     const [showAppointmentModal, setAppointmentModal] = useState(false);
+    const [showAppointmentForm, setAppointmentForm] = useState(false);
     const [appointModalTitle, setAppointModalTitle] = useState("");
-    const [AppointmentDetails, setAppointmentDetails] = useState([]);
+    const [AppointmentDetails, setAppointmentDetails] = useState({});
 
-    const onClickShow = () =>{
+
+    const [listpets, setListPets] = useState([]);
+
+    let user_id = localStorage.getItem("user_id");
+
+    useEffect(()=>{
+        async function fetchData(user_id){
+            let json = await API_POST("listpets/" + user_id);
+            setListPets(json.data);
+        }
+        fetchData(user_id);
+                   
+    },[])
+
+
+    const onShowAppointment = () =>{
         setAppointmentModal(true);
         setAppointModalTitle("รายละเอียดการนัดหมาย")
         setAppointmentDetails(props.data);
     }
 
+    const onShowAppointmentForm = () => {
+        setAppointmentForm(true);
+        setAppointmentDetails(props.data);
+        setAppointModalTitle("แก้ไขการนัดหมาย");
+    }
+
     const onClose = () =>{
         setAppointmentModal(false);
+        setAppointmentForm(false);
     }
 
     return (
@@ -32,13 +56,13 @@ import { Button } from "react-bootstrap";
                 <td><p>{props.data.appoint_status}</p></td>
                 <td>
                     <div>
-                        <Button onClick={onClickShow} className="btn btn-success me-3">{<i className="fa-regular fa-eye me-2"></i>}รายละเอียด</Button>
+                        <Button onClick={onShowAppointment} className="btn btn-success me-3">{<i className="fa-regular fa-eye me-2"></i>}รายละเอียด</Button>
                     </div>
                 </td>
                 <td>
                     {appoint_status == "รอแก้ไข" &&
                         <div>
-                            <Link to={`/account/appointment/${props.data.appoint_id}`} className="btn btn-warning me-3">{<i className="fa-solid fa-pen-to-square me-2"></i>}แก้ไข</Link>
+                            <Button onClick={onShowAppointmentForm} className="btn btn-warning me-3">{<i className="fa-solid fa-pen-to-square me-2"></i>}แก้ไข</Button>
                         </div>
                     }
                 </td>
@@ -48,6 +72,14 @@ import { Button } from "react-bootstrap";
             title={appointModalTitle}
             onClose={onClose}
             data={AppointmentDetails}
+            />
+
+            <ShowAppointmentForm 
+            show={showAppointmentForm}
+            title={appointModalTitle}
+            onClose={onClose}
+            data={AppointmentDetails}
+            pets={listpets}
             />
         </>
     )
