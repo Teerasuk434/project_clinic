3f899ac8d328fe7dc4809e586f67c9d8132ec51c
ปาริฉัตร ละม้ายสกุล <lamai.p2000@gmail.com>
2022-09-26 18:14:44 +0700

    up

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -48,7 +48,6 @@ import AppointSchedule from './components/owner/AppointSchedule';
 import ListAppoint from './components/ListAppoint';
 import HistoryAppoint from './components/employee/HistoryAppoint';
 
-import Dashboard from './components/owner/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -99,7 +98,6 @@ root.render(
       <Route path="/emp/:emp_id" element={<FormEmployee/>} />
       <Route path="/history-appoint" element={<HistoryAppoint/>} />
       
-      <Route path="dashboard" element={<Dashboard/>} />
 
 
     </Routes> 
