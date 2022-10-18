0d17d304bceebf5b88f9b8d6d0ac2807e3f696e0
ปาริฉัตร ละม้ายสกุล <lamai.p2000@gmail.com>
2022-10-18 15:54:40 +0700

    up

--- my-app/src/AboutServiceItem.js
+++ my-app/src/AboutServiceItem.js
@@ -6,8 +6,8 @@ export default function AboutServicesItems(props){
         <>
             <div className="row box-service  mb-4 shadow">
                 
-                <div className=" col-3 m-auto "><b><img src={`http://localhost:8080/images/${props.data.service_image}` } className="mx-3" width={50}/>  {props.data.service_name}</b></div>
-                <div className=" col-3 m-auto "><b>{props.data.cost_service}</b></div>
+                <div className=" col-4 m-auto "><b><img src={`http://localhost:8080/images/${props.data.service_image}` } className="mx-3" width={50}/>  {props.data.service_name}</b></div>
+                <div className=" col-2 m-auto "><b>{props.data.cost_service}</b></div>
                 <div className=" col-3 m-auto "><b>{props.data.cost_deposit}</b></div>
                 <div className=" col-3 m-auto "><b>{props.data.time_spent}</b></div>
                         
